a7dc1deebccd1c290744937834003e7919493662
matzryo <matzryo@gmail.com>
2023-04-17 11:06:41 +0900

    WIP: feat: GraphQL リゾルバ実装 TIHU-47
    リポジトリ分割のために、いったんコミットします。

--- apps/backend/dist/src/index.js
+++ apps/backend/dist/src/index.js
@@ -4,45 +4,39 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { fileURLToPath } from 'url';
-import { mtgCardResolvers } from '../resolvers/query.js';
-
+import { mtgCardResolvers } from '../resolvers/query';
 // apps/backend/schemas/*.graphql をスキーマとして読み込む
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const schemaPath = path.join(__dirname, "../schemas/*.graphql");
-const typesArray = loadFilesSync<string>(schemaPath);
+const typesArray = loadFilesSync(schemaPath);
 const mergedTypeDefs = mergeTypeDefs(typesArray);
-
 const books = [
     {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+        title: 'The Awakening',
+        author: 'Kate Chopin',
     },
     {
-      title: 'City of Glass',
-      author: 'Paul Auster',
+        title: 'City of Glass',
+        author: 'Paul Auster',
     },
-  ];
-
-  // Resolvers define how to fetch the types defined in your schema.
+];
+// Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     ...mtgCardResolvers,
-  };
-
+};
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers,
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
+});
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+});
+console.log(`🚀  Server ready at: ${url}`);
